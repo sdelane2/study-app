529511e81385d37a0431434a1cf1478dcc4dc2ba
sdelane2 <seanmartindelaney@gmail.com>
2022-04-05 14:47:50 -0400

    Add additional fields to courses

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,7 @@
 class Course < ApplicationRecord
-    validates :title, presence: true
+    validates :title, :short_description, :language, :price, :level, presence: true
     validates :description, presence: true, length: { :minimum => 5 }
+
     def to_s
         title
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,13 @@
-User.create!(email: "admin@example.com", password: "adminexample", password_confirmation: "adminexample")
+# User.create!(email: "admin@example.com", password: "adminexample", password_confirmation: "adminexample")
 
 30.times do
     Course.create!([{
         title: Faker::Educator.course_name,
         description: Faker::Quotes::Shakespeare.hamlet_quote,
-        user_id: User.first.id
+        user_id: User.first.id,
+        short_description: Faker::Quote.jack_handey,
+        language: Faker::ProgrammingLanguage.name,
+        level: "Beginner",
+        price: Faker::Number.between(from: 1000, to: 2000)
     }])
 end
